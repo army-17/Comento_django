00654aa0094866860208270724a321acf1d6e64c
army-17 <j2665138@naver.com>
2022-05-22 00:59:26 +0900

    캠페인 상세페이지 url 문제 발생

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import MainContent
 
@@ -9,3 +9,9 @@ def index(request):
     content_list = MainContent.objects.order_by('-pub_date')  #- 붙이면 역순으로
     context = {'content_list': content_list}
     return render(request, 'mysite/content_list.html', context)
+
+def detail(request, content_id):
+
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list':content_list}
+    return render(request, 'mysite/content_detail.html', context)
