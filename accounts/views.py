435447e7e5fd041094d75907670dd54e60d50935
army-17 <j2665138@naver.com>
2022-05-26 23:12:08 +0900

    accounts 기능 추가(오류 존재)

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from accounts.forms import SignupForm
+
+
+# Create your views here.
+
+def signup(request):
+    if request.method =="POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+
+        else:
+            form = SignupForm()
+
+        return render(request, 'accounts/signup.html',{'form':form})
