cc54dda2770b686707d452d72ad5ec929ea0a845
army-17 <j2665138@naver.com>
2022-05-27 12:57:40 +0900

    회원가입 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -6,14 +6,17 @@ from accounts.forms import SignupForm
 # Create your views here.
 
 def signup(request):
-    if request.method =="POST":
+    if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('/')
 
-        else:
-            form = SignupForm()
+    else:
+        form = SignupForm()
 
-        return render(request, 'accounts/signup.html',{'form':form})
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def logout(request):
+        logout(request)
